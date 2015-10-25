121c438138a3e9bcab4e165d8f62b3eac22529b3
Nico Bellante <nicobellante@gmail.com>
2015-10-25 06:26:24 -0400

    logger and parser classes

---
+++ TestLogger.py
@@ -0,0 +1,20 @@
+import csv
+import getpass
+import os.path
+
+
+class TestLogger():
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def add_entry_to_log(self, entry_dict):
+        log_name = getpass.getuser() + '-' + self.file_name + '-log.csv'
+
+        already_existed = os.path.isfile(log_path + '/' + log_name)
+
+        with open(log_name, 'a') as csvfile:
+            fieldnames = ['timestamp', 'commit_id', 'result', 'cycles', 'instructions', 'registers_used']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            if not already_existed:
+                writer.writeheader()
+            writer.writerow(entry_dict)
